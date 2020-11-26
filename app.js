17a2953015030cdd742beb72bc1370ccbf670ab6
drakola211211 <sekakoko98765@gmail.com>
2020-11-26 07:19:12 +0200

    new website

---
+++ app.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const body = document.querySelector('body');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+// Animations
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('animate-hero', {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3
+})
+
+gsap.from('animate-services', {
+    ScrollTrigger: './animate-service',
+    duration: 0.5,
+    opacity: 1,
+    x: -150,
+    stagger: 0.12
+})
+
+gsap.from('animate-img', {
+    ScrollTrigger: './animate-service',
+    duration: 1.2,
+    opacity: 0,
+    x: -200,
+    stagger: 0.12
+})
